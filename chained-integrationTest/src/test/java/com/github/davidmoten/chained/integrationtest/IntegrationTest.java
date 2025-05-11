d8288f2a50393748e746502994cf43c2389b0b6e
Dave Moten <davidmoten@gmail.com>
2025-05-11 15:11:05 +1000

    dev

--- chained-integrationTest/src/test/java/com/github/davidmoten/chained/integrationtest/IntegrationTest.java
+++ chained-integrationTest/src/test/java/com/github/davidmoten/chained/integrationtest/IntegrationTest.java
@@ -1,16 +1,12 @@
 package com.github.davidmoten.chained.integrationtest;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class IntegrationTest {
 
     @Test
     public void testValidUsage() {
-
-        GeneratedClass generatedClass = new GeneratedClass();
-
+        new GeneratedClass();
     }
 
 }
