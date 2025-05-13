5741fd7136b0b522db070313bf5a3b91af21a4ce
Dave Moten <davidmoten@gmail.com>
2025-05-13 12:08:42 +1000

    dev

---
+++ chained-api/src/main/java/com/github/davidmoten/chained/api/BuilderConstructor.java
@@ -0,0 +1,14 @@
+package com.github.davidmoten.chained.api;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.CONSTRUCTOR})
+@Documented
+public @interface BuilderConstructor {
+
+}
