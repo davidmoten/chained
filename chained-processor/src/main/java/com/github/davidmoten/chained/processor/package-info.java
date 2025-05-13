9acb94dc823d608a532b2dbb10ba0811f827a1fc
Dave Moten <davidmoten@gmail.com>
2025-05-13 17:42:41 +1000

    dev

--- chained-processor/src/main/java/com/github/davidmoten/chained/processor/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains the chained annotation processor.
- */
-@AnnotationWrapper(value={Builder.class})
-package com.github.davidmoten.chained.processor;
-
-import com.github.davidmoten.chained.api.Builder;
-import io.toolisticon.aptk.annotationwrapper.api.AnnotationWrapper;
