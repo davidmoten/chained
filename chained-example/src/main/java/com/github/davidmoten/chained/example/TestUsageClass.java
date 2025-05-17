05a295c7fb1fef0b3fea6879b08ea69cf2ebdc92
Dave Moten <davidmoten@gmail.com>
2025-05-18 08:51:08 +1000

    dev

--- chained-example/src/main/java/com/github/davidmoten/chained/example/TestUsageClass.java
+++
@@ -1,9 +0,0 @@
-package com.github.davidmoten.chained.example;
-
-public class TestUsageClass {
-
-    public static void main(String[] args) {
-        @SuppressWarnings("unused")
-        TestBean instance = TestBean.number(123L).name("abc").description("hello").build();
-    }
-}
