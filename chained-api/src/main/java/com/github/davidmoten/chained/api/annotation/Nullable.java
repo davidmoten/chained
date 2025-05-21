a68ac0083f8228d164287acb856d417257818dc0
Dave Moten <davidmoten@gmail.com>
2025-05-21 17:04:33 +1000

    dev

--- chained-api/src/main/java/com/github/davidmoten/chained/api/annotation/Nullable.java
+++ chained-api/src/main/java/com/github/davidmoten/chained/api/annotation/Nullable.java
@@ -1,11 +1,10 @@
 package com.github.davidmoten.chained.api.annotation;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 @Target(value = { ElementType.PARAMETER })
 public @interface Nullable {
 
