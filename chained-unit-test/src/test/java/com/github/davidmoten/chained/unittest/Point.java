8a53a94241e47b1dc408003cddee5939803896c3
Dave Moten <davidmoten@gmail.com>
2025-08-01 15:01:02 +1000

    dev

--- chained-unit-test/src/test/java/com/github/davidmoten/chained/unittest/Point.java
+++ chained-unit-test/src/test/java/com/github/davidmoten/chained/unittest/Point.java
@@ -1,6 +1,8 @@
 package com.github.davidmoten.chained.unittest;
 
 import com.github.davidmoten.chained.api.annotation.Builder;
+import com.github.davidmoten.chained.unittest.builder.PointBuilder;
+import com.github.davidmoten.chained.unittest.builder.PointBuilder.BuilderWithX;
 
 /**
  * Represents a point in 2D space.
@@ -9,4 +11,9 @@ import com.github.davidmoten.chained.api.annotation.Builder;
  * @param y the y-coordinate of the point
  */
 @Builder
-public record Point(int x, int y) {}
+public record Point(int x, int y) {
+    
+    public static BuilderWithX x(int x) {
+        return PointBuilder.builder().x(x);
+    } 
+}
